bb91dc893793a8c1341cb0ccc756a7c438fb7f52
Matt Tesauro <mtesauro@gmail.com>
2015-03-23 16:33:34 -0500

    Updated the POC client to use the newly added error handling aka multi-value returns from some method calls

--- examples/client-poc/client-poc.go
+++ examples/client-poc/client-poc.go
@@ -3,24 +3,34 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/mtesauro/tfclient"
+	"os"
 )
 
 func main() {
 	fmt.Println("POC ThreadFix Go Client")
 	// First Create a client for talking to the API
-	tfc := tfclient.CreateClient()
-	fmt.Printf("tfc is %+v \n", tfc)
+	tfc, err := tfclient.CreateClient()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+	fmt.Printf("\ntfc - the client connection is: \n %+v \n", tfc)
 	// Before searching you must setup a default Search Struct
 	srch := tfclient.CreateSearchStruct()
 	// Restrict default search to only 4 results
 	tfclient.NumSearchResults(&srch, 4)
 	// Send the search query to TF
-	vulns := tfclient.VulnSearch(tfc, &srch)
+	vulns, err := tfclient.VulnSearch(tfc, &srch)
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
 	// Create a search struct and load it with the search with just conducted
 	var search tfclient.SrchResp
+	//fmt.Printf("\nvulns is %+v\n\n", vulns)
+	//os.Exit(0)
 	tfclient.MakeSearchStruct(&search, vulns)
-	fmt.Printf("search is %+v \n", search)
+	fmt.Printf("\nSearch results are:\n %+v \n\n", search)
 
 }
