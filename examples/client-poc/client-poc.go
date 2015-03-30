df2479ddcf2f7f5f057a9b309e8adaa04020acd8
Matt Tesauro <mtesauro@gmail.com>
2015-03-30 14:47:19 -0500

    Updated client-poc in examples directory to use the CreateEmptyConfig function if a config file cannot be found

--- examples/client-poc/client-poc.go
+++ examples/client-poc/client-poc.go
@@ -5,32 +5,43 @@ import (
 	"fmt"
 	"github.com/mtesauro/tfclient"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("POC ThreadFix Go Client")
+	fmt.Println("POC ThreadFix Go Client\n")
+
 	// First Create a client for talking to the API
 	tfc, err := tfclient.CreateClient()
 	if err != nil {
 		fmt.Print(err)
+		if strings.Contains(err.Error(), "Unable to find config file") {
+			tfclient.CreateEmptyConfig()
+			fmt.Println("An default config file named tfclient.config was created in the current directory")
+			fmt.Println("Please update that file with the values for your ThreadFix installation\n")
+		}
 		os.Exit(1)
 	}
 	fmt.Printf("\ntfc - the client connection is: \n %+v \n", tfc)
+
 	// Before searching you must setup a default Search Struct
 	srch := tfclient.CreateSearchStruct()
+
 	// Restrict default search to only 4 results
 	tfclient.NumSearchResults(&srch, 4)
+
 	// Send the search query to TF
 	vulns, err := tfclient.VulnSearch(tfc, &srch)
 	if err != nil {
 		fmt.Print(err)
 		os.Exit(1)
 	}
+
 	// Create a search struct and load it with the search with just conducted
 	var search tfclient.SrchResp
-	//fmt.Printf("\nvulns is %+v\n\n", vulns)
-	//os.Exit(0)
 	tfclient.MakeSearchStruct(&search, vulns)
+
+	// Print the search struct
 	fmt.Printf("\nSearch results are:\n %+v \n\n", search)
 
 }
